b7e440cf81d919e01841836ac9f488932ab756c5
Vivek <pawarvivek29@gmail.com>
2017-11-28 03:19:03 +0530

    arrayList

---
+++ arrayList.js
@@ -0,0 +1,31 @@
+class ArrayList {
+  constructor(){
+    this.length = 0;
+    this.data = {};
+  }
+  push(value){
+    this.data[this.length] = value;
+    this.length++;
+  }
+  pop(){
+    const ans = this.data[this.length-1];
+    delete this.data[this.length-1];
+    this.length--;
+    return ans;
+  }
+  get(index){
+    return this.data[index];
+  }
+  delete(index){
+    const ans = this.data[index];
+    this._collapseTo(index);
+    return ans;
+  }
+  _collapseTo(index){
+    for(var i = index; i < data.length; i++){
+      this.data[i] = this.data[i+1];
+    }
+    delete this.data[this.length-1];
+    this.length--;
+  }
+}
